7880255b1a3e6ba6dbf610e55528e78d30c70348
mythreya <mythreya.jl@gmail.com>
2018-09-24 21:52:45 -0700

    Formatting the files to my liking

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -20,7 +20,7 @@ FusionEKF::FusionEKF() {
   R_laser_ = MatrixXd(2, 2);
   R_radar_ = MatrixXd(3, 3);
   H_laser_ = MatrixXd(2, 4);
-  Hj_ = MatrixXd(3, 4);
+  Hj_      = MatrixXd(3, 4);
 
   //measurement covariance matrix - laser
   R_laser_ << 0.0225,      0,
@@ -34,9 +34,9 @@ FusionEKF::FusionEKF() {
   H_laser_ << 1, 0, 0, 0,
               0, 1, 0, 0;
     
-  Hj_ << 0, 0, 0, 0,
-         0, 0, 0, 0,
-         0, 0, 0, 0;
+  Hj_      << 0, 0, 0, 0,
+              0, 0, 0, 0,
+              0, 0, 0, 0;
     
   
   /*
--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -12,44 +12,44 @@ KalmanFilter::~KalmanFilter() {}
 
 void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
                         MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
-  x_ = x_in;
-  P_ = P_in;
-  F_ = F_in;
-  H_ = H_in;
-  R_ = R_in;
-  Q_ = Q_in;
+    x_ = x_in;
+    P_ = P_in;
+    F_ = F_in;
+    H_ = H_in;
+    R_ = R_in;
+    Q_ = Q_in;
 }
 
 void KalmanFilter::Predict() {
-  x_ = F_ * x_;
-  P_ = F_ * P_ * F_.transpose() + Q_;
+    x_ = F_ * x_;
+    P_ = F_ * P_ * F_.transpose() + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-  VectorXd z_pred = H_ * x_;
-  VectorXd y = z - z_pred;
-  MatrixXd Ht = H_.transpose();
-  MatrixXd S = H_ * P_ * Ht + R_;
-  MatrixXd PHt = P_ * Ht;
-  MatrixXd K = PHt * S.inverse();
+    VectorXd z_pred = H_ * x_;
+    VectorXd y = z - z_pred;
+    MatrixXd Ht = H_.transpose();
+    MatrixXd S = H_ * P_ * Ht + R_;
+    MatrixXd PHt = P_ * Ht;
+    MatrixXd K = PHt * S.inverse();
 
-  x_ = x_ + K * y;
-  long x_size = x_.size();
-  MatrixXd I = MatrixXd::Identity(x_size, x_size);
-  P_ = (I - K * H_) * P_;
+    x_ = x_ + K * y;
+    long x_size = x_.size();
+    MatrixXd I = MatrixXd::Identity(x_size, x_size);
+    P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
-  auto& Hj = H_;
-  VectorXd z_pred = Hj * x_;
-  VectorXd y = z - z_pred;
-  MatrixXd Ht = Hj.transpose();
-  MatrixXd S = Hj * P_ * Hj + R_;
-  MatrixXd PHt = P_ * Hj;
-  MatrixXd K = PHt * S.inverse();
-  
-  x_ = x_ + K * y;
-  long x_size = x_.size();
-  MatrixXd I = MatrixXd::Identity(x_size, x_size);
-  P_ = (I - K * Hj) * P_;
+    auto& Hj = H_;
+    VectorXd z_pred = Hj * x_;
+    VectorXd y = z - z_pred;
+    MatrixXd Ht = Hj.transpose();
+    MatrixXd S = Hj * P_ * Hj + R_;
+    MatrixXd PHt = P_ * Hj;
+    MatrixXd K = PHt * S.inverse();
+
+    x_ = x_ + K * y;
+    long x_size = x_.size();
+    MatrixXd I = MatrixXd::Identity(x_size, x_size);
+    P_ = (I - K * Hj) * P_;
 }
